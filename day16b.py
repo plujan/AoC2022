307f19eb771c3e9494998671859de76a38d799c4
Paul Lujan <plujan314@gmail.com>
2025-10-22 14:07:08 -0700

    go back to original method

--- day16b.py
+++ day16b.py
@@ -2,15 +2,12 @@
 
 import re
 
-# An attempt at optimizing our solution for part 2. The idea here is: when we're at a valve and choosing what
-# to try next, instead of trying all unopened valves, we go through and reject any valve that is both farther
-# away and has a smaller flow rate than another valve in the unopened set. Rather than do this computation
-# every time, we calculate a lookup table once (which does require going through all 2^16 combinations of
-# unopened valves) at the beginning and then use that.
-
-# The good news is that this reduces the time significantly, to <10 seconds. The bad news is that this
-# optimization is too aggressive -- it apparently causes us to miss the best solution. Back to the drawing
-# board!
+# This is a first attempt to port the "new" solution for part 1 to part 2. It uses basically the same method,
+# but with moving both you and the elephant.
+#
+# Unfortunately, while this solves the sample problem nearly instantly still, the full problem increases the
+# number of combinations enough that we don't get close to finishing in a reasonable time. Time to think some
+# more...
 
 max_time = 26
 
@@ -31,7 +28,7 @@ with open("day16.txt") as infile:
                 useful_valves.append(valve_name)
         else:
             print("Found malformed line", line)
-useful_valves = sorted(useful_valves)
+
 
 # Now, find the shortest path between each pair of valves. We have to do this for all valves, not just
 # the useful valves, since we start on a non-useful valve.
@@ -67,53 +64,9 @@ for v1 in valves.keys():
 
 # print(valve_distance)
 
-start_valve = 'AA'
-
-print("Building optimal valve table, one sec...")
-optimal_valves = {}
-# Now, build a table of optimal connections for each possible set of valves.
-for x in range(1, 2**len(useful_valves)):
-    # Iterate over all possible combinations of valves remaining. We can skip 0 because there's nothing to do
-    # if no valves are left.
-    current_valve_set = []
-    for i in range(len(useful_valves)):
-        if (x >> i) % 2 == 1:
-            current_valve_set.append(useful_valves[i])
-
-    # Consider each valve as a possible starting point, unless all valves are closed, in which case we're at
-    # the start point
-    test_valve_set = list(useful_valves)
-    if len(current_valve_set) == len(useful_valves):
-        test_valve_set = [start_valve]
-    current_valve_key = "".join(current_valve_set)
-    
-    # Now, iterate over all starting points
-    for v1 in test_valve_set:
-        if v1 in current_valve_set:
-            continue
-        # For this starting point and this set of open valves, we want to find valves that are both
-        # farther away and have a lower flow rate than another valve in the set, and discard them, in
-        # hopes of reducing our possible number of combinations
-        result_valves = []
-        
-        for v2 in current_valve_set:
-            save_this_valve = True
-            for v3 in current_valve_set:
-                if v2 == v3:
-                    continue
-                if valve_distance[v1][v2] > valve_distance[v1][v3] and valves[v2][0] < valves[v3][0]:
-                    save_this_valve = False
-                    break
-            if save_this_valve:
-                result_valves.append(v2)
-        # Okay, we're done, save results & move on
-        if current_valve_key not in optimal_valves:
-            optimal_valves[current_valve_key] = {}
-        optimal_valves[current_valve_key][v1] = result_valves
-    # print(current_valve_set, optimal_valves[current_valve_key])
-    
 # Now, let's get to actual solving.
 state_pool = []
+start_valve = 'AA'
 
 # Each element of the state pool has the following format:
 # - current set of UNOPENED valves
@@ -125,25 +78,20 @@ state_pool = []
 # - next target for elephant
 # - distance to next target for elephant
 
-# We probably could do something clever to account for the fact that swapping us and the elephant
-# results in the same solution, but this seems like a lot of work
-for v1 in optimal_valves["".join(useful_valves)][start_valve]:
-    for v2 in optimal_valves["".join(useful_valves)][start_valve]:
-        if v1 == v2:
-            continue
-        state_pool.append((set(useful_valves), 0, 0, 0, v1, valve_distance[start_valve][v1],
-                           v2, valve_distance[start_valve][v2]))
+# Since swapping us ant the elephant results in the same solution, we can divide the number of start states in
+# half
+for i1 in range(len(useful_valves)):
+    for i2 in range(i1+1, len(useful_valves)):
+        state_pool.append((set(useful_valves), 0, 0, 0, useful_valves[i1], valve_distance[start_valve][useful_valves[i1]],
+                           useful_valves[i2], valve_distance[start_valve][useful_valves[i2]]))
 
 current_best = 0
 n_proc = 0
 while len(state_pool) > 0:
     n_proc += 1
-    if (n_proc % 100000) == 0:
-        print("Processed", n_proc, "states; size of state pool is", len(state_pool))
-        if n_proc > 10e6:
-            sys.exit(1)
-        #print(state_pool)
-        #sys.exit(1)
+    if (n_proc % 1e6) == 0:
+        print("Processed %0e states; size of state pool is %d" % (n_proc, len(state_pool)))
+
     (cur_valves, cur_t, cur_score, cur_flow, next_target_us, target_distance_us,
      next_target_ele, target_distance_ele) = state_pool.pop()
     
@@ -168,8 +116,6 @@ while len(state_pool) > 0:
         cur_score += cur_flow*target_distance_us
         cur_flow += valves[next_target_us][0]
         cur_flow += valves[next_target_ele][0]
-
-    cur_valves_key = "".join(sorted(cur_valves))
         
     if cur_t >= max_time:
         # We're done, correct our score for the stuff we shouldn't have gotten
@@ -178,8 +124,21 @@ while len(state_pool) > 0:
             current_best = cur_score
     else:
         if len(cur_valves) == 0:
-            # We've opened all valves; just increase the score as appropriate and finish up.
+            # We've opened all valves; just increase the score as appropriate and finish up. This is a little
+            # more complicated because if we've finished for one mover but not the other, we need to account
+            # for their last move too.
             cur_score += cur_flow*(max_time-cur_t)
+            if target_distance_us < target_distance_ele:
+                extra_t = cur_t + (target_distance_ele-target_distance_us)
+                if extra_t < max_time:
+                    cur_score += valves[next_target_ele][0]*(max_time-extra_t)
+            elif target_distance_ele < target_distance_us:
+                extra_t = cur_t + (target_distance_us-target_distance_ele)
+                if extra_t < max_time:
+                    cur_score += valves[next_target_us][0]*(max_time-extra_t)
+            # Fortunately we don't have to deal with the case where they both finish at the same time, since
+            # in that case everyone is accounted for.
+            
             if cur_score > current_best:
                 current_best = cur_score
         else:
@@ -189,8 +148,8 @@ while len(state_pool) > 0:
                 if len(cur_valves) == 1:
                     state_pool.append((set(cur_valves), cur_t, cur_score, cur_flow, start_valve, 999999,
                                        next_target_ele, target_distance_ele-target_distance_us))
-
-                for v in optimal_valves[cur_valves_key][next_target_us]:
+                
+                for v in cur_valves:
                     if v == next_target_ele:
                         continue
                     state_pool.append((set(cur_valves), cur_t, cur_score, cur_flow, v, valve_distance[next_target_us][v],
@@ -200,7 +159,7 @@ while len(state_pool) > 0:
                     state_pool.append((set(cur_valves), cur_t, cur_score, cur_flow, next_target_us,
                                        target_distance_us-target_distance_ele, start_valve, 999999))
                 
-                for v in optimal_valves[cur_valves_key][next_target_ele]:
+                for v in cur_valves:
                     if v == next_target_us:
                         continue
                     state_pool.append((set(cur_valves), cur_t, cur_score, cur_flow, next_target_us,
@@ -210,7 +169,7 @@ while len(state_pool) > 0:
                 # because of the criterion that we send us and the elephant to different places. So send whoever's closest
                 # there and the other person nowhere.
                 
-                for v1 in optimal_valves[cur_valves_key][next_target_us]:
+                for v1 in cur_valves:
                     if len(cur_valves) == 1:
                         if valve_distance[next_target_us][v1] <= valve_distance[next_target_ele][v1]:
                             state_pool.append((set(cur_valves), cur_t, cur_score, cur_flow, v1,
@@ -219,7 +178,7 @@ while len(state_pool) > 0:
                             state_pool.append((set(cur_valves), cur_t, cur_score, cur_flow, start_valve, 999999,
                                                v1, valve_distance[next_target_ele][v1]))
 
-                    for v2 in optimal_valves[cur_valves_key][next_target_ele]:
+                    for v2 in cur_valves:
                         if v1 == v2:
                             continue
                         state_pool.append((set(cur_valves), cur_t, cur_score, cur_flow, v1,
